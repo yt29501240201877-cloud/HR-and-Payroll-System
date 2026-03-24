de8348e28df082084285171fe102dd7624f14f83
youseftawfik <youssefsafwat95@yahoo.com>
2026-03-24 11:31:23 +0200

    CORS

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require("cors");
 
 const app = express();
-
+app.use(cors());
 
 app.use(express.json());
 
